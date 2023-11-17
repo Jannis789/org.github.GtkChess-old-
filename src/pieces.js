a5cd50e30461491cae96c8b5f17ec761868ade39
Jannis Rustige <jannis@fedora.fritz.box>
2023-11-17 22:48:15 +0100

    organize & update code

--- src/pieces.js
+++ src/pieces.js
@@ -1,11 +1,13 @@
 import Gio from 'gi://Gio';
 import Gtk from 'gi://Gtk';
 import GdkPixbuf from 'gi://GdkPixbuf';
+
 import {
     fetchTile,
     updateChessBoard,
     printGameBoard
 }
+
 from './game-board.js';
 let whiteKing = [];
 let blackKing = [];
@@ -19,32 +21,6 @@ const fullPieceTerm = {
 }
 const pieces = {};
 let isWhitesTurn = true;
-
-function checkCheck(checkWhitesKing) {
-    // Überprüft, ob die gültigen Züge den gegnerischen König bedrohen.
-    let isInCheck = false;
-    const enemyKing = checkWhitesKing ? blackKing : whiteKing; // Referenz auf den gegnerischen König, da der Zug noch nicht beendet ist
-    Object.values(pieces).flat().forEach(piece => {
-        if (piece.isWhitePiece === checkWhitesKing) {
-            const enemyValidMoves = piece.possibleMoves;
-            isInCheck = enemyValidMoves.some(threateningPiece => threateningPiece[0] === enemyKing[0] && threateningPiece[1] === enemyKing[1]) || isInCheck;
-        }   
-    });
-    return isInCheck;
-}
-
-function kingIsInCheck(x,y) {
-    let isInCheck = false;
-    const targetedPiece = getPiece(x, y);
-
-    Object.values(pieces).flat().forEach(piece => {
-        if (piece.isWhitePiece !== targetedPiece.isWhitePiece) {
-            const enemyValidMoves = piece.pieceType.toLowerCase() === 'p' ? piece.attackPositions : piece.possibleMoves;
-            isInCheck = enemyValidMoves.some(threateningPiece => threateningPiece[0] === x && threateningPiece[1] === y) || isInCheck;
-        }
-    });
-    return isInCheck;
-}
 
 function getPiece(x, y) {
     return pieces[x]?.[y];
@@ -60,6 +36,34 @@ function definePiece(object, x, y) {
     pieces[x] = pieces[x] || [];
     pieces[x][y] = object;
 }
+
+function initPiece(pieceType, x, y) {
+    const button = fetchTile(x, y);
+    if (pieceType === null) {
+        button.set_child(null);
+        return button;
+    }
+    const piecePrefix = pieceType.toUpperCase() === pieceType ? 'white_' : 'black_'
+    const resourcePath = '/org/github/GtkChess/img/' + piecePrefix + fullPieceTerm[pieceType.toLowerCase()] + '.svg';
+    const file = Gio.File.new_for_uri('resource://' + resourcePath);
+    const inputStream = file.read(null);
+    const image = new Gtk.Image();
+    const pixbuf = GdkPixbuf.Pixbuf.new_from_stream_at_scale(inputStream, 400, 400, true, null);
+    image.set_from_pixbuf(pixbuf);
+    button.set_child(image);
+    return button;
+}
+
+/**
+ * positionCallback is a callback object with a Function,
+ * the function handleChessPiece will be triggered every time a chess tile is clicked.
+ */
+
+const positionCallback = {
+    handleChessPiece: function(x, y) {
+        performMove(x, y);
+    }
+};
 
 class piece { // Base class for every Chess Piece
     constructor(pieceType, x, y) {
@@ -174,6 +178,28 @@ export class pawn extends piece {
     }
 }
 
+function getPossibleMoves(isWhitePiece, x, y, movementPattern) {
+    const validMoves = [];
+    for (const [xOffset, yOffset] of movementPattern) {
+        let offsetMultiplier = 1;
+        while (true) {
+            const [newX, newY] = [x + xOffset * offsetMultiplier, y + yOffset * offsetMultiplier];
+            const newPiece = getPiece(newX, newY);
+            const outOfBounds = newX < 0 || newX > 7 || newY < 0 || newY > 7;
+            // Break the loop if the new coordinates are out of bounds or if a piece of the same color is encountered
+            if (outOfBounds || (newPiece && newPiece.isWhitePiece === isWhitePiece)) {
+                break;
+            }
+            validMoves.push([newX, newY]); // Add the new coordinates to the valid moves array
+            // Break the loop if a piece of a different color is encountered
+            if (newPiece && newPiece.isWhitePiece !== isWhitePiece) {
+                break;
+            }
+            offsetMultiplier++;
+        }
+    }
+    return validMoves;
+}
 
 export class rook extends piece {
     constructor(pieceType, x, y) {
@@ -336,67 +362,36 @@ export class king extends piece {
     }
 }
 
-function initPiece(pieceType, x, y) {
-    const button = fetchTile(x, y);
-    if (pieceType === null) {
-        button.set_child(null);
-        return button;
-    }
-    const piecePrefix = pieceType.toUpperCase() === pieceType ? 'white_' : 'black_'
-    const resourcePath = '/org/github/GtkChess/img/' + piecePrefix + fullPieceTerm[pieceType.toLowerCase()] + '.svg';
-    const file = Gio.File.new_for_uri('resource://' + resourcePath);
-    const inputStream = file.read(null);
-    const image = new Gtk.Image();
-    const pixbuf = GdkPixbuf.Pixbuf.new_from_stream_at_scale(inputStream, 400, 400, true, null);
-    image.set_from_pixbuf(pixbuf);
-    button.set_child(image);
-    return button;
-}
-
-function getPossibleMoves(isWhitePiece, x, y, movementPattern) {
-    const validMoves = [];
-    for (const [xOffset, yOffset] of movementPattern) {
-        let offsetMultiplier = 1;
-        while (true) {
-            const [newX, newY] = [x + xOffset * offsetMultiplier, y + yOffset * offsetMultiplier];
-            const newPiece = getPiece(newX, newY);
-            const outOfBounds = newX < 0 || newX > 7 || newY < 0 || newY > 7;
-            // Break the loop if the new coordinates are out of bounds or if a piece of the same color is encountered
-            if (outOfBounds || (newPiece && newPiece.isWhitePiece === isWhitePiece)) {
-                break;
-            }
-            validMoves.push([newX, newY]); // Add the new coordinates to the valid moves array
-            // Break the loop if a piece of a different color is encountered
-            if (newPiece && newPiece.isWhitePiece !== isWhitePiece) {
-                break;
-            }
-            offsetMultiplier++;
+function checkCheck(checkWhitesKing) {
+    // Überprüft, ob die gültigen Züge den gegnerischen König bedrohen.
+    let isInCheck = false;
+    const enemyKing = checkWhitesKing ? blackKing : whiteKing; // Referenz auf den gegnerischen König, da der Zug noch nicht beendet ist
+    Object.values(pieces).flat().forEach(piece => {
+        if (piece.isWhitePiece === checkWhitesKing) {
+            const enemyValidMoves = piece.possibleMoves;
+            isInCheck = enemyValidMoves.some(threateningPiece => threateningPiece[0] === enemyKing[0] && threateningPiece[1] === enemyKing[1]) || isInCheck;
         }
-    }
-    return validMoves;
+    });
+    return isInCheck;
 }
 
-/** 
- * positionCallback is a callback object with a Function,
- * the function handleChessPiece will be triggered every time a chess tile is clicked.
- */
-
-const positionCallback = {
-    handleChessPiece: function(x, y) {
-        performMove(x, y);
-    }
-};
-
-let previousMoves = [];
-let previousPiece = [];
+function kingIsInCheck(x,y) {
+    let isInCheck = false;
+    const targetedPiece = getPiece(x, y);
+    Object.values(pieces).flat().forEach(piece => {
+        if (piece.isWhitePiece !== targetedPiece.isWhitePiece) {
+            const enemyValidMoves = piece.pieceType.toLowerCase() === 'p' ? piece.attackPositions : piece.possibleMoves;
+            isInCheck = enemyValidMoves.some(threateningPiece => threateningPiece[0] === x && threateningPiece[1] === y) || isInCheck;
+        }
+    });
+    return isInCheck;
+}
 
 function movesWhichWontLeadToCheck(x, y) {
-    
-    const [startX, startY] = [x, y];
-    const illegalMoves = [];
-    const deletedPieces = [];
     let piece = getPiece(x, y);
+    const deletedPieces = [];
     const possibleMoves = piece.possibleMoves;
+    const isKing = piece.pieceType.toLowerCase() === 'k';
     const finalMoves = [];
 
     for (const [possibleX, possibleY] of possibleMoves) {
@@ -411,16 +406,16 @@ function movesWhichWontLeadToCheck(x, y) {
 
         // You only want to restore the pieces which were hittable if they are not under attack (!targetPiece)
         // Look, if opponent's piece (which is replaced by the King) is capable of being attacked, it's necessary due to King must be protected at any cost
-        if (!targetPiece || (piece.pieceType.toLowerCase() === 'k' && kingIsInCheck(possibleX, possibleY))) {
+        if (!targetPiece || (isKing && kingIsInCheck(possibleX, possibleY))) {
             for (const deletedPiece of deletedPieces) {
                 initPiece(deletedPiece.pieceType, deletedPiece.x, deletedPiece.y);
                 definePiece(deletedPiece, deletedPiece.x, deletedPiece.y);
             }
         }
         // if piece leads to check, respectively check mate
-        if (piece.pieceType.toLowerCase() === 'k' && !kingIsInCheck(possibleX, possibleY)) {
+        if (isKing && !kingIsInCheck(possibleX, possibleY)) {
             finalMoves.push([possibleX, possibleY]);
-        } else if  (!checkCheck(!isWhitesTurn)) {
+        } else if  (!isKing && !checkCheck(!isWhitesTurn)) {
             finalMoves.push([possibleX, possibleY]);
         }
         // remove simulated piece
@@ -439,6 +434,23 @@ function movesWhichWontLeadToCheck(x, y) {
     return finalMoves;
 }
 
+function selectPossibleMoves(possiblePositions) {
+    for (const [x, y] of possiblePositions) {
+        const buttonContext = fetchTile(x, y).get_style_context();
+        buttonContext.add_class('possible-position');
+    }
+}
+
+function unselectMoves(previousPosition) {
+    for (const [x, y] of previousPosition) {
+        const buttonContext = fetchTile(x, y).get_style_context();
+        buttonContext.remove_class('possible-position');
+    }
+}
+
+let previousMoves = [];
+let previousPiece = [];
+
 function performMove(x, y) {
     const piece = getPiece(x, y);
     // if a piece is encountered
@@ -456,20 +468,6 @@ function performMove(x, y) {
         isWhitesTurn = !isWhitesTurn;
         previousMoves = [];
         previousPiece = [];
-    }
-}
-
-function selectPossibleMoves(possiblePositions) {
-    for (const [x, y] of possiblePositions) {
-        const buttonContext = fetchTile(x, y).get_style_context();
-        buttonContext.add_class('possible-position');
-    }
-}
-
-function unselectMoves(previousPosition) {
-    for (const [x, y] of previousPosition) {
-        const buttonContext = fetchTile(x, y).get_style_context();
-        buttonContext.remove_class('possible-position');
     }
 }
 
